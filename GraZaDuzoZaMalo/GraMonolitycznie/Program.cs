a92aef87f9c99a432f232c85e44bc57be780bed3
maciek436 <48645613+maciek436@users.noreply.github.com>
2019-03-17 12:25:20 +0100

    Wersja 1

---
+++ GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraMonolitycznie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj");
+
+            Console.WriteLine("Podaj swoje imie: ");
+            string imie = Console.ReadLine();
+            Console.WriteLine($"Witaj {imie}!!!");
+        }
+    }
+}
